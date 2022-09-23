de3de7a823eb2b644675f7da789b431c4c6a61fd
gabriel <gabrielalmeidads@gmail.com>
2022-09-22 22:55:10 -0300

    Updating sidebar component

--- src/components/side-bar/index.js
+++ src/components/side-bar/index.js
@@ -16,30 +16,45 @@ function SideBar() {
             </div>
             <SideBarItem icon={icons.gears}/>
           </div>
-          <div>
+          <div className={style.sideBarMenuItemLabel}>
             <span>Configurações</span>
           </div>
         </div>
+
         <div className={style.sideBarMenuItem}>
           <div className={style.sideBarMenuItemIcons}>
             <div>
             </div>
             <SideBarItem icon={icons.share}/>
           </div>
-          <div>
+          <div className={style.sideBarMenuItemLabel}>
             <span>Compartilhamento</span>
           </div>
         </div>
-        <div className={style.sideBarMenuItem}>
-          <div className={style.sideBarMenuItemIcons}>
-            <div>
-              {/* <SideBarItem icon={icons.caretRight} /> */}
-              <SideBarItem icon={icons.caretDown} />
-            </div>
-            <SideBarItem icon={icons.fileArchive}/>
-          </div>
+
+        <div className={`${style.sideBarMenuItem} ${style.sideBarMenuItemSubLis}`}>
           <div>
-            <span>Relatórios</span>
+            <div className={style.sideBarMenuItemIcons}>
+              <div>
+                {/* <SideBarItem icon={icons.caretRight} /> */}
+                <SideBarItem icon={icons.caretDown} />
+              </div>
+              <SideBarItem icon={icons.fileArchive}/>
+            </div>
+
+            <div className={style.sideBarMenuItemLabel}>
+              <span>Relatórios</span>
+            </div>
+          </div>
+
+          <div className={style.subList}>
+            <ul>
+              <li>Usuários e Perfis</li> 
+              <li>Operacionais</li>
+              <li>Financeiros e Faturamento</li> 
+              <li>Auditoria</li>
+              <li>Estoque e Suprimentos</li>
+            </ul>
           </div>
         </div>
       </div>
